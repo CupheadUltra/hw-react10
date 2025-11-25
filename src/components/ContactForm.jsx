48df75e6789c630f4eb6b9a5aaa387a13ce295db
CupheadUltra <129087076+CupheadUltra@users.noreply.github.com>
2025-11-25 20:28:25 +0200

    ready

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.name, this.state.number);
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces."
+            required
+          />
+        </label>
+
+        <label>
+          Number
+          <input
+            type="tel"
+            name="number"
+            value={this.state.number}
+            onChange={this.handleChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits..."
+            required
+          />
+        </label>
+
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ContactListItem from './ContactListItem';
+
+const ContactList = ({ contacts, onDelete }) => {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => (
+        <ContactListItem
+          key={id}
+          id={id}
+          name={name}
+          number={number}
+          onDelete={onDelete}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
