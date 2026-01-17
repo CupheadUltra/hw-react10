2e93c528223de759156b9c3610e18e912d492a16
CupheadUltra <129087076+CupheadUltra@users.noreply.github.com>
2026-01-17 17:47:27 +0200

    localStorage

--- src/App.js
+++ src/App.js
@@ -7,14 +7,28 @@ import './App.css';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+  
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+
+    if (savedContacts) {
+      this.setState({
+        contacts: JSON.parse(savedContacts),
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(
+        'contacts',
+        JSON.stringify(this.state.contacts)
+      );
+    }
+  }
 
   addContact = (name, number) => {
     const normalized = name.toLowerCase();
@@ -24,7 +38,7 @@ class App extends Component {
     );
 
     if (exists) {
-      alert(`${name} уже есть в телефонной книге`);
+      alert(`${name} вже є у телефонній книзі`);
       return;
     }
 
@@ -75,7 +89,7 @@ class App extends Component {
             onDelete={this.deleteContact}
           />
         ) : (
-          <p>Contacts not found</p>
+          <p>Контакти не знайдено</p>
         )}
       </div>
     );
